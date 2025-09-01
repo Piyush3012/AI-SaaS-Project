8dc1af7b453238b2b9dc6077467375e0b2cfb089
piyush deshpande <piyushdeshpande003@gmail.com>
2025-09-01 18:50:11 +0530

    host binding

--- server/server.js
+++ server/server.js
@@ -26,6 +26,6 @@ app.use('/api/ai',aiRouter);
 app.use('/api/user',userRouter)
 const port=process.env.PORT || 3000;
 
-app.listen(port,()=>{
+app.listen(port,"0.0.0.0",()=>{
     console.log(`Server is running on port ${port}`)
 })
